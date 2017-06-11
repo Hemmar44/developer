bc3458e97866d6649584847e90a0a3cf44bd5f48
Hemmar44 <hedrzak.marcin@gmail.com>
2017-06-11 11:37:55 +0200

    404 error handling

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.layout')
+
+@section('content')
+	<header id="head" class="secondary"></header>
+
+	<!-- container -->
+	<div class="container">
+
+		<h3 class='text-muted'>Oops!!!</h3>
+
+		<div class="row">
+			
+			<!-- Article main content -->
+			<article class="col-sm-12 maincontent">
+				<header class="page-header">
+					<h1 class="page-title">Wybrana strona nie istnieje </h1>
+				</header>
+				
+				
+			</article>
+			<!-- /Article -->
+			
+			
+
+		</div>
+	</div>	<!-- /container -->
+	
+	@endsection
